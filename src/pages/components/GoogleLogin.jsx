e5fc98499bbddca9d4e4dd0f60f278c15831edd4
galist1304 <135500174+galist1304@users.noreply.github.com>
2024-02-28 14:26:49 +0200

    changed google  client id

--- src/pages/components/GoogleLogin.jsx
+++ src/pages/components/GoogleLogin.jsx
@@ -1,17 +1,14 @@
 import React, { useEffect } from 'react'
 import { jwtDecode } from "jwt-decode"
-import { useContexts } from '../../useContext/ContextsProvider'
-import Cookies from 'js-cookie';
 import useLogin from '../../hooks/useLogin';
 
 const GoogleLogin = () => {
-    const { setLoggedUser } = useContexts()
     const { googleLogin } = useLogin()
 
     useEffect(() => {
         /* global google */
         google.accounts.id.initialize({
-          client_id: "785666814116-6s043aehflp0dj85fiu7un9fqlak91h4.apps.googleusercontent.com",
+          client_id: "785666814116-6s043aehflp0dj85fiu7un9fqlak91h4.apps.googleusercontent.",
           callback: handleCallbackResponse
         })
         google.accounts.id.renderButton(
@@ -23,7 +20,6 @@ const GoogleLogin = () => {
       const handleCallbackResponse = (response) => {
         const googleToken = jwtDecode(response.credential)
         console.log(googleToken);
-        //setLoggedUser({ username: googleToken.name, role: 'user' });
         const userData = { username: googleToken.name, role: 'user' };
         googleLogin(userData)
       }
